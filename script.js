b6b9996f88543b7695fb0c1183a7cc617e50c367
kzharkovskyi <kyrylo.zharkovskyi@smartfactor.pl>
2025-11-02 11:35:11 +0100

    cloudflared tunnel --url http://localhost:3000 and cloudflared tunnel --url http://localhost:8000

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
-const statusUrl = "https://api.allorigins.win/raw?url=" + encodeURIComponent("http://localhost:8000/status-json.xsl");
+const playButton = document.getElementById('playButton');
+const radioPlayer = document.getElementById('radioPlayer');
+const trackElement = document.getElementById('current-track'); // element, gdzie wyświetlamy tytuł
+let playing = false;
 
-const trackElement = document.getElementById("current-track");
-
-// async function fetchTrackInfo() {
-//   try {
-//     const response = await fetch(statusUrl);
-//     const data = await response.json();
-
-//     const source = Array.isArray(data.icestats.source)
-//       ? data.icestats.source[0]
-//       : data.icestats.source;
-
-//     if (source && source.title) {
-//       trackElement.textContent = "🎵 Teraz gra: " + source.title;
-//     } else {
-//       trackElement.textContent = "🔇 Brak informacji o utworze.";
-//     }
-//   } catch (error) {
-//     console.error("Błąd pobierania statusu Icecast:", error);
-//     trackElement.textContent = "⚠️ Nie można pobrać informacji o utworze.";
-//   }
-// }
-
-// fetchTrackInfo();
-// setInterval(fetchTrackInfo, 5000);
-
-const button = document.getElementById('playButton');
-const player = document.getElementById('radioPlayer');
-
-button.addEventListener('click', () => {
-  player.play();
-  button.style.display = 'none'; // ukrywa przycisk po starcie
+// Obsługa przycisku play/pause
+playButton.addEventListener('click', () => {
+  if (!playing) {
+    radioPlayer.play();
+    playButton.innerHTML = '<i class="fa-solid fa-pause"></i>'; // ikona pauzy
+  } else {
+    radioPlayer.pause();
+    playButton.innerHTML = '<i class="fa-solid fa-play"></i>'; // ikona play
+  }
+  playing = !playing;
 });
 
+// Funkcja pobierająca aktualnie grający utwór z Icecast
+async function updateTrack() {
+  try {
+    // const res = await fetch('http://localhost:8000/status-json.xsl'); // lub Twój tunel Cl0oudflared
+    const res = await fetch('https://utc-uniform-seq-atlanta.trycloudflare.com/status-json.xsl'); // lub Twój tunel Cloudflared
+    const data = await res.json();
+
+    // Dla pojedynczego źródła
+    const source = data.icestats.source;
+    const title = source?.title || 'Brak informacji o utworze';
+
+    trackElement.textContent = `${title}`;
+  } catch (err) {
+    console.error('Błąd pobierania tracka:', err);
+    trackElement.textContent = '🎵 Błąd połączenia z Icecast';
+  }
+}
+
+// Odświeżaj co 5 sekund
+setInterval(updateTrack, 5000);
+updateTrack();
